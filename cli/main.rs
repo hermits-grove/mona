4c058c2989222fbf26d408901ca2180e5a220a82
David Rusu <davidrusu.me@gmail.com>
2018-03-12 03:35:10 -0400

    refactored some things

--- cli/main.rs
+++ cli/main.rs
@@ -11,11 +11,10 @@ use std::fs::File;
 use std::error::Error;
 use std::io::Read;
 use std::io::Write;
+use std::io::stdout;
 
 extern crate ring;
 use ring::{aead, digest, pbkdf2};
-use ring::rand::SecureRandom;
-extern crate data_encoding;
 
 mod secret_meta;
 
@@ -53,26 +52,6 @@ fn setup_repo() -> git2::Repository{
     repo.unwrap()
 }
 
-fn decode(encoded: &String, meta: &secret_meta::Meta) -> Vec<u8> {
-    let encoding = match meta.binary_encoding.as_ref() {
-        "base64url" => data_encoding::BASE64URL,
-        s => panic!("Unknonw encoding algorithm: {}", s)
-    };
-    let decoded = match encoding.decode(encoded.as_bytes()) {
-        Ok(decoded) => decoded,
-        Err(err) => panic!("Failed to decode string: {:?}", err)
-    };
-    decoded
-}
-
-fn encode(data: &Vec<u8>, meta: &secret_meta::Meta) -> String {
-    let encoding = match meta.binary_encoding.as_ref() {
-        "base64url" => data_encoding::BASE64URL,
-        s => panic!("Unknonw encoding algorithm: {}", s)
-    };
-    encoding.encode(data)
-}
-
 fn pbkdf2(pass: &[u8], keyleng: u32, pbkdf2_meta: &secret_meta::PBKDF2) -> Vec<u8> {
     if keyleng < 128 {
         panic!("key is too short! keyleng (bits): {}", keyleng);
@@ -90,57 +69,31 @@ fn encrypt<'a>(pass: &[u8], data: &[u8], meta: &secret_meta::Meta) -> Result<Vec
     if meta.aead.algo != "ChaCha20-Poly1305" {
         panic!("only 'ChaCha20-Poly1305' implemented for aead");
     }
-    
-    println!("finished sanity checks");
+    if meta.aead.keylen % 8 != 0 {
+        panic!("keylen must be a multiple of 8!");
+    }
     
     let pbkdf2_algo = &digest::SHA256;
     let aead_algo = &aead::CHACHA20_POLY1305;
 
-    if meta.aead.keylen % 8 != 0 {
-        panic!("keylen must be a multiple of 8!");
-    }
+    let salt = meta.decode(&meta.pbkdf2.salt);
     let mut key = vec![0u8; (meta.aead.keylen / 8) as usize];
-    pbkdf2::derive(pbkdf2_algo, meta.pbkdf2.iters, &decode(&meta.pbkdf2.salt, &meta), &pass, &mut key);
-    
-    println!("finished pbkdf2 {} {}", key.len(), aead_algo.key_len());
-    
+    pbkdf2::derive(pbkdf2_algo, meta.pbkdf2.iters, &salt, &pass, &mut key);
     let seal_key = aead::SealingKey::new(aead_algo, &key).unwrap();
-
-    println!("created seal key");
-    let mut in_out = Vec::new();
+    
+    let mut in_out = Vec::with_capacity(data.len() + seal_key.algorithm().tag_len());
     in_out.extend(data.iter());
     in_out.extend(vec![0u8; seal_key.algorithm().tag_len()]);
-
     let ad: &[u8] = &toml::to_vec(&meta).unwrap();
-    
-    println!("finished ad setup");
-    let nonce = &decode(&meta.aead.nonce, &meta);
-    println!("Nonce: {} {:?}", nonce.len(), nonce);
+    let nonce = &meta.decode(&meta.aead.nonce);
 
-    
-    println!("nonce: {:?}", nonce);
-    println!("ad: {:?}", ad);
-    println!("in_out: {:?}", in_out);
-    let res = aead::seal_in_place(
-        &seal_key,
-        &nonce,
-        &ad,
-        &mut in_out,
-        seal_key.algorithm().tag_len()
-    );
-    
-    println!("finished seal");
-
-    match res {
-        Ok(size) => {
-            println!("sealed: Encrypted File Size {}", size);
-            Ok(in_out)
-        }
-        Err(e) => {
-            println!("Error sealing: {:?}", e);
-            Err(String::from("Failed to seal"))
-        }
-    }   
+    aead::seal_in_place(&seal_key,
+                        &nonce,
+                        &ad,
+                        &mut in_out,
+                        seal_key.algorithm().tag_len())
+        .map_err(|e| String::from("Failed to seal"))
+        .map(|_| in_out)
 }
 
 fn decrypt<'a>(pass: &[u8], encrypted_data: &[u8], meta: &secret_meta::Meta) -> Result<Vec<u8>, String> {
@@ -150,163 +103,88 @@ fn decrypt<'a>(pass: &[u8], encrypted_data: &[u8], meta: &secret_meta::Meta) -> 
     if meta.aead.algo != "ChaCha20-Poly1305" {
         panic!("only 'ChaCha20-Poly1305' implemented for aead");
     }
-    
-    println!("finished sanity checks");
+    if meta.aead.keylen % 8 != 0 {
+        panic!("keylen must be a multiple of 8!");
+    }
     
     let pbkdf2_algo = &digest::SHA256;
     let aead_algo = &aead::CHACHA20_POLY1305;
 
-    if meta.aead.keylen % 8 != 0 {
-        panic!("keylen must be a multiple of 8!");
-    }
+    let salt = &meta.decode(&meta.pbkdf2.salt);
     let mut key = vec![0u8; (meta.aead.keylen / 8) as usize];
-    pbkdf2::derive(pbkdf2_algo, meta.pbkdf2.iters, &decode(&meta.pbkdf2.salt, &meta), &pass, &mut key);
-    
-    println!("finished pbkdf2 {} {}", key.len(), aead_algo.key_len());
-    
+    pbkdf2::derive(pbkdf2_algo, meta.pbkdf2.iters, &salt, &pass, &mut key);
     let opening_key = aead::OpeningKey::new(aead_algo, &key).unwrap();
-
-    println!("created open key");
-    let ad: &[u8] = &toml::to_vec(&meta).unwrap();
-    println!("finished ad setup");
-    let nonce = &decode(&meta.aead.nonce, &meta);
-    println!("Nonce: {} {:?}", nonce.len(), nonce);
 
     let mut in_out = Vec::new();
     in_out.extend(encrypted_data.iter());
-    //println!("Key: {:?}", opening_key);
-    println!("nonce: {:?}", nonce);
-    println!("ad: {:?}", ad);
-    println!("in_out: {:?}", in_out);
-    let res =  aead::open_in_place(&opening_key, &nonce, &ad, 0, &mut in_out);
-    
-    println!("finished open");
-    match res {
-        Ok(plaintext) => {
-            println!("opened");
-            Ok(plaintext.to_vec())
-        }
-        Err(e) => {
-            println!("Error opening: {:?}", e);
-            Err(String::from("Failed to open"))
-        }
-    }
+
+    let ad: &[u8] = &toml::to_vec(&meta).unwrap();
+    let nonce = &meta.decode(&meta.aead.nonce);
+
+    aead::open_in_place(&opening_key, &nonce, &ad, 0, &mut in_out)
+        .map_err(|_| String::from("Failed to open"))
+        .map(|plaintext| plaintext.to_vec())
 }
 
-fn arg_encrypt_file(in_file: &path::Path, out_file: &path::Path) {
-    let default_meta = secret_meta::from_file(path::Path::new("./default_meta.toml"))
-        .unwrap();
-    println!("{:?}", default_meta);
-    
-    // setup up salt and nonce
-    let mut salt = vec![0u8; 96 / 8 ];
-    let mut nonce = vec![0u8; 96 / 8];
-    
-    let rng = ring::rand::SystemRandom::new();
-    rng.fill(&mut salt)
-        .map_err(|e| panic!("Failed to fill salt with rand"));
-    rng.fill(&mut nonce)
-        .map_err(|e| panic!("Failed to fill nonce with rand"));
-
-    let encoded_salt = encode(&salt, &default_meta);
-    let encoded_nonce = encode(&nonce, &default_meta);
-    println!("encoded_salt: {}", encoded_salt);
-    println!("salt: {:?}", salt);
-    println!("encoded_nonce: {}", encoded_nonce);
-    println!("nonce: {:?}", nonce);
-    
-    let meta = secret_meta::Meta {
-        pbkdf2: secret_meta::PBKDF2 {
-            salt: encoded_salt,
-            ..default_meta.pbkdf2.clone()
-        },
-        aead: secret_meta::AEAD {
-            nonce: encoded_nonce,
-            ..default_meta.aead.clone()
-        },
-        ..default_meta.clone()
-    };
-    
-    let encrypted_contents = File::open(in_file)
-        .map_err(|e| format!("Failed to open {:?}: {}", in_file, e.description()))
-        .and_then(|mut f| {
-            let mut contents = Vec::new();
-            f.read_to_end(&mut contents)
-                .map_err(|e| format!("Failed to read {:?}: {}", in_file, e.description()))
-                .and_then(|_| {
-                    let mut pass = String::new();
-                    match io::stdin().read_line(&mut pass) {
-                        Ok(n) => {
-                            println!("{} bytes read", n);
-                            println!("{}", pass);
-                        }
-                        Err(error) => println!("error reading password: {}", error),
-                    }
-                    encrypt(pass.as_bytes(), &contents, &meta)
-                })
-        });
-
-    let meta_path = out_file.with_extension("toml");
-    let meta_res = File::create(&meta_path)
-        .map_err(|e| format!("Failed to create {:?}: {}", meta_path, e.description()))
-        .and_then(|mut f| {
-            toml::to_vec(&meta)
-                .map_err(|e| format!("Failed to serialize meta {:?}", e))
-                .and_then(|serialized_meta| {
-                    f.write_all(&serialized_meta)
-                        .map_err(|e| format!("Failed to write meta file {:?}: {}", meta_path, e.description()))
-                })
-        });
-    println!("meta file: {:?}", meta_res);
-
-    let encrypted_res = File::create(out_file)
-        .map_err(|e| format!("Failed to create {:?}: {}", out_file, e.description()))
-        .and_then(|mut f| {
-            encrypted_contents
-                .and_then(|data| {
-                    f.write_all(&data)
-                        .map_err(|e| format!("Failed to write {:?}: {}", out_file, e.description()))
-                })
-        });
-    
-    println!("encrypted file: {:?}", encrypted_res);
+fn read_password_stdin() -> Result<String, String> {
+    // TODO: for unix systems, do something like this: https://stackoverflow.com/a/37416107
+    print!("master passphrase: ");
+    stdout().flush();
+    let mut pass = String::new();
+    io::stdin().read_line(&mut pass)
+        .map_err(|e| format!("Error reading password from stdin: {}", e))
+        .map(|_| pass)
 }
 
-fn arg_decrypt_file(encrypted_file: &path::Path, plaintext_file: &path::Path) {
-    let meta = secret_meta::from_file(&encrypted_file.with_extension("toml"))
-        .unwrap();
-    println!("{:?}", meta);
-    
-    let plaintext_contents = File::open(encrypted_file)
-        .map_err(|e| format!("Failed to open {:?}: {}", encrypted_file, e.description()))
-        .and_then(|mut f| {
-            let mut contents = Vec::new();
-            f.read_to_end(&mut contents)
-                .map_err(|e| format!("Failed to read {:?}: {}", encrypted_file, e.description()))
-                .and_then(|_| {
-                    let mut pass = String::new();
-                    match io::stdin().read_line(&mut pass) {
-                        Ok(n) => {
-                            println!("{} bytes read", n);
-                            println!("{}", pass);
-                        }
-                        Err(error) => println!("error reading password: {}", error),
-                    }
-                    decrypt(pass.as_bytes(), &contents, &meta)
-                })
-        });
+fn arg_encrypt_file(plaintext_file: &path::Path, encrypted_file: &path::Path) -> Result<(), String> {
+    let meta = secret_meta::Meta::generate_secure_meta()?;
 
-    let plaintext_res = File::create(plaintext_file)
-        .map_err(|e| format!("Failed to create {:?}: {}", plaintext_file, e.description()))
+    let plaintext_data = File::open(plaintext_file)
+        .map_err(|e| format!("Failed to open {:?}: {:?}", plaintext_file, e))
         .and_then(|mut f| {
-            plaintext_contents
-                .and_then(|data| {
-                    f.write_all(&data)
-                        .map_err(|e| format!("Failed to write {:?}: {}", plaintext_file, e.description()))
-                })
-        });
+            let mut data = Vec::new();
+            f.read_to_end(&mut data)
+                .map_err(|e| format!("Failed read {:?}: {:?}", plaintext_file, e))
+                .map(|_| data)
+        })?;
     
-    println!("plaintext file: {:?}", plaintext_res);
+    let encrypted_data = read_password_stdin()
+        .and_then(|pass| encrypt(pass.as_bytes(), &plaintext_data, &meta))?;
+
+    // write encrypted file to disk
+    File::create(encrypted_file)
+        .map_err(|e| format!("Failed to create {:?}: {:?}", encrypted_file, e))
+        .and_then(|mut f| {
+            f.write_all(&encrypted_data)
+                .map_err(|e| format!("Failed write to {:?}: {:?}", encrypted_file, e))
+        })?;
+
+    // write encryption metadata to disk
+    meta.write_file(&encrypted_file.with_extension("toml"))
+}
+
+fn arg_decrypt_file(encrypted_file: &path::Path, plaintext_file: &path::Path) -> Result<(), String> {
+    let meta = secret_meta::Meta::from_file(&encrypted_file.with_extension("toml"))?;
+    
+    let encrypted_data = File::open(encrypted_file)
+        .map_err(|e| format!("Failed to open {:?}: {:?}", encrypted_file, e))
+        .and_then(|mut f| {
+            let mut encrypted_data = Vec::new();
+            f.read_to_end(&mut encrypted_data)
+                .map_err(|e| format!("Failed to read {:?}: {:?}", encrypted_file, e))
+                .map(|_| encrypted_data)
+        })?;
+
+    let plaintext_data = read_password_stdin()
+        .and_then(|pass| decrypt(pass.as_bytes(), &encrypted_data, &meta))?;
+
+    // write plaintext file to disk
+    File::create(plaintext_file)
+        .map_err(|e| format!("Failed to create {:?}: {:?}", plaintext_file, e))
+        .and_then(|mut f| {
+            f.write_all(&plaintext_data)
+                .map_err(|e| format!("Failed to write {:?}: {:?}", plaintext_file, e))
+        })
 }
 
 fn main() {
@@ -333,49 +211,25 @@ fn main() {
                          .help("Destination for plaintext_file")))
         .get_matches();
 
-    if let Some(matches) = matches.subcommand_matches("encrypt") {
-        let plaintext_file = path::Path::new(matches.value_of("plaintext_file").unwrap());
-        let encrypted_file = path::Path::new(matches.value_of("encrypted_file").unwrap());
-        println!("plaintext_file {:?}, encrypted_file {:?}", plaintext_file, encrypted_file);
-        arg_encrypt_file(&plaintext_file, &encrypted_file);
+    match matches.subcommand() {
+        ("encrypt", Some(sub_m)) => {
+            let plaintext_file = path::Path::new(sub_m.value_of("plaintext_file").unwrap());
+            let encrypted_file = path::Path::new(sub_m.value_of("encrypted_file").unwrap());
+            match arg_encrypt_file(&plaintext_file, &encrypted_file) {
+                Ok(_) => println!("File encrypted"),
+                Err(e) => println!("Failed to encrypt file:\n{}", e)
+            };
+        },
+        ("decrypt", Some(sub_m)) => {
+            let encrypted_file = path::Path::new(sub_m.value_of("encrypted_file").unwrap());
+            let plaintext_file = path::Path::new(sub_m.value_of("plaintext_file").unwrap());
+            match arg_decrypt_file(&encrypted_file, &plaintext_file) {
+                Ok(_) => println!("File decrypted"),
+                Err(e) => println!("Failed to decrypt file:\n{}", e)
+            }
+        }
+        _ => {
+            println!("{}", matches.usage());
+        }
     }
-    
-
-    if let Some(matches) = matches.subcommand_matches("decrypt") {
-        let encrypted_file = path::Path::new(matches.value_of("encrypted_file").unwrap());
-        let plaintext_file = path::Path::new(matches.value_of("plaintext_file").unwrap());
-        println!("plaintext_file {:?}, encrypted_file {:?}", plaintext_file, encrypted_file);
-        arg_decrypt_file(&encrypted_file, &plaintext_file);
-    }
-    
-    //println!("{:?}", matches);
-    //
-    //let algo = &aead::CHACHA20_POLY1305;
-    //let password = "password";
-    //let salt: &[u8] = &[0u8; 96 / 8];
-    //let pbkdf2_iterations = 10000;
-//
-    //let ad: &[u8] = &[];
-    //let nonce: &[u8] = &[0u8; 96 / 8]; // 96 bit nonce
-    //let data = &String::from("this is a test").into_bytes()[..];
-    //
-    //let mut key: [u8; 256 / 8] = [0u8; 256 / 8];
-//
-    //pbkdf2::derive(&digest::SHA256, pbkdf2_iterations, salt, password.as_bytes(), &mut key);
-    //let sealing_key = aead::SealingKey::new(&algo, &key).unwrap();
-    //let sig_tag_len: usize = sealing_key.algorithm().tag_len();
-    //let mut in_out = vec![0u8; data.len() + sig_tag_len];
-    //in_out.extend(data.iter());
-//
-    //match aead::seal_in_place(&sealing_key, &nonce, &ad, &mut in_out, sig_tag_len) {
-    //    Ok(size) => println!("sealed: Size {}", size),
-    //    Err(e) => println!("Error sealing: {:?}", e)
-    //};
-    //println!("in_out: {:?}", in_out);
-//
-    //let opening_key = aead::OpeningKey::new(&algo, &key).unwrap();
-    //match aead::open_in_place(&opening_key, &nonce, &ad, 0, &mut in_out) {
-    //    Ok(text) => println!("{:?}", String::from_utf8(text.to_vec())),
-    //    Err(e) => println!("Error opening: {:?}", e)
-    //};
 }
--- cli/secret_meta.rs
+++ cli/secret_meta.rs
@@ -1,7 +1,14 @@
 use std::fs::File;
 use std::io::Read;
+use std::io::Write;
 use std::path::Path;
 use std::error::Error;
+
+use ring;
+use ring::rand::SecureRandom;
+
+extern crate data_encoding;
+
 use toml;
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -39,17 +46,78 @@ pub struct Paranoid {
     pub cascading_encryption: String
 }
 
+impl Meta {
+    pub fn generate_secure_meta() -> Result<Meta, String> {
+        let default_meta = Meta::from_file(Path::new("./default_meta.toml"))?;
+        
+        let mut salt = vec![0u8; 96 / 8 ];
+        let mut nonce = vec![0u8; 96 / 8];
+        let rng = ring::rand::SystemRandom::new();
+        rng.fill(&mut salt)
+            .map_err(|e| format!("Failed to generate secure salt: {:?}", e))?;
+        rng.fill(&mut nonce)
+            .map_err(|e| format!("Failed to generate secure nonce: {:?}", e))?;
 
-pub fn from_file(path: &Path) -> Result<Meta, String> {
-    File::open(path)
-        .map_err(|e| format!("Failed to open {:?}: {}", path, e.description()))
-        .and_then(|mut f| {
-            let mut contents = Vec::new();
-            f.read_to_end(&mut contents)
-                .map_err(|e| format!("Failed to read {:?}: {}", path, e.description()))
-                .map(|_| contents)
-        })
-        .and_then(|contents| {
-            toml::from_slice(&contents).map_err(|e| format!("{:?}", e))
-        })
+        // TODO: check generated nonce against burned_nonces
+
+        let meta = Meta {
+            pbkdf2: PBKDF2 {
+                salt: default_meta.encode(&salt),
+                ..default_meta.pbkdf2.clone()
+            },
+            aead: AEAD {
+                nonce: default_meta.encode(&nonce),
+                ..default_meta.aead.clone()
+            },
+            ..default_meta.clone()
+        };
+        Ok(meta)
+    }
+
+    pub fn from_file(path: &Path) -> Result<Meta, String> {
+        File::open(path)
+            .map_err(|e| format!("Failed to open {:?}: {:?}", path, e))
+            .and_then(|mut f| {
+                let mut contents = Vec::new();
+                f.read_to_end(&mut contents)
+                    .map_err(|e| format!("Failed to read {:?}: {:?}", path, e))
+                    .map(|_| contents)
+            })
+            .and_then(|contents| {
+                toml::from_slice(&contents).map_err(|e| format!("{:?}", e))
+            })
+    }
+
+    pub fn write_file(&self, path: &Path) -> Result<(), String> {
+        toml::to_vec(&self)
+            .map_err(|e| format!("Failed to serialize meta {:?}", e))
+            .and_then(|serialized_meta| {
+                File::create(&path)
+                    .map_err(|e| format!("Failed to create {:?}: {:?}", path, e))
+                    .and_then(|mut f| {
+                        f.write_all(&serialized_meta)
+                            .map_err(|e| format!("Failed write to meta file {:?}: {:?}", path, e))
+                    })
+            })
+    }
+
+    pub fn decode(&self, encoded: &String) -> Vec<u8> {
+        let encoding = match self.binary_encoding.as_ref() {
+            "base64url" => data_encoding::BASE64URL,
+            s => panic!("Unknonw encoding algorithm: {}", s)
+        };
+        let decoded = match encoding.decode(encoded.as_bytes()) {
+            Ok(decoded) => decoded,
+            Err(err) => panic!("Failed to decode string: {:?}", err)
+        };
+        decoded
+    }
+
+    pub fn encode(&self, data: &Vec<u8>) -> String {
+        let encoding = match self.binary_encoding.as_ref() {
+            "base64url" => data_encoding::BASE64URL,
+            s => panic!("Unknonw encoding algorithm: {}", s)
+        };
+        encoding.encode(data)
+    }
 }
